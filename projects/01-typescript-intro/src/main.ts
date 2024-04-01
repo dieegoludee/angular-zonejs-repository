efdef28c9eca4dee49f88607e15364ffc354f689
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-01 23:40:19 +0200

    Uso de funciones

--- projects/01-typescript-intro/src/main.ts
+++ projects/01-typescript-intro/src/main.ts
@@ -1,6 +1,9 @@
 import './style.css'
 // import './topics/01-basic-types'
-import './topics/02-object-interface'
+// import './topics/02-object-interface'
+// import './topics/03-functions'
+import './topics/04-types'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
