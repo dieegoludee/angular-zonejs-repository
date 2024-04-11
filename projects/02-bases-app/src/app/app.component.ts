660c63faa89121099069d6832a640e77f1e08310
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-11 23:25:10 +0200

    Creación de contador

--- projects/02-bases-app/src/app/app.component.ts
+++ projects/02-bases-app/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'bases-app-angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  decreaseBy(value: number): void {
+    this.counter -= value;
+    if (this.counter < 0) {
+      this.counter = 0;
+    }
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
