a510c917dd364c339117c00b9d5e25088ea9ab03
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-19 12:07:43 +0200

    Paquete externo - UUID - Parte 1

--- projects/02-bases-app/src/app/dbz/services/dbz.service.ts
+++ projects/02-bases-app/src/app/dbz/services/dbz.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({
@@ -9,14 +11,17 @@ export class DbzService {
 
   public characters: Character[] = [
     {
+      id: uuid(),
       name: 'Krillin',
       power: 1000,
     },
     {
+      id: uuid(),
       name: 'Goku',
       power: 9500,
     },
     {
+      id: uuid(),
       name: 'Vegeta',
       power: 7500,
     },
