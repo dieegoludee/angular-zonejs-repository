9d412183c55b3b08436101b766c7be7931d684d5
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-21 13:52:41 +0200

    @ViewChild - Referencia al HTML

--- projects/03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ projects/03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css',
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  constructor() {}
+
+  @ViewChild('txtTagInput')
+  // ! not null operator
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag(newTag: string): void {
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
+}
