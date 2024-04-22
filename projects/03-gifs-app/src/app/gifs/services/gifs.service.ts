be15302ad0cd377c21566aedccf21c4b142bf93c
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-23 00:04:15 +0200

    Colocar un tipo de dato a una respuesta HTTP y mostrando gifs por pantalla

--- projects/03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ projects/03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 const GIPHY_API_KEY: string = 'QsvIS7Hd5yzVDwOB4ifpXNFShVKWG9aD';
 
@@ -7,6 +8,8 @@ const GIPHY_API_KEY: string = 'QsvIS7Hd5yzVDwOB4ifpXNFShVKWG9aD';
   providedIn: 'root',
 })
 export class GifsService {
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
   private apiKey: string = GIPHY_API_KEY;
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
@@ -20,25 +23,31 @@ export class GifsService {
   private organizeHistory(tag: string): void {
     tag = tag.toLowerCase();
 
+    // Si el historial de tags contiene 2 valores iguales, el filter devuelve una lista sin valores repetidos
     if (this._tagsHistory.includes(tag)) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
 
     this._tagsHistory.unshift(tag);
-    this._tagsHistory = this.tagsHistory.splice(0, 10); // PARA LIMITAR A 10
+    this._tagsHistory = this.tagsHistory.splice(0, 10); // Para limitar a 10 los valores de la lista
   }
 
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
 
+    // Parámetros de la URL de la API con la API Key, un límite de elementos a recibir y la query con el tag
     const params: HttpParams = new HttpParams()
       .set('api_key', this.apiKey)
       .set('limit', 10)
       .set('q', tag);
 
-    this.http.get(`${this.serviceUrl}/search`, { params }).subscribe((resp) => {
-      console.log(resp);
-    });
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        // console.log(resp);
+        this.gifList = resp.data;
+        console.log({ gifs: this.gifList });
+      });
   }
 }
