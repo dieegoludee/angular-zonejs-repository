2ae9efbac81f5f019a0dea7e09f916efb9aac4b3
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-22 18:32:02 +0200

    Uso de API Key de Giphy y realización de petición HTTP con Observables

--- projects/03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ projects/03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,17 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const GIPHY_API_KEY: string = 'QsvIS7Hd5yzVDwOB4ifpXNFShVKWG9aD';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   private _tagsHistory: string[] = [];
+  private apiKey: string = GIPHY_API_KEY;
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -26,7 +31,14 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
-    // this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory);
+
+    const params: HttpParams = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params }).subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
