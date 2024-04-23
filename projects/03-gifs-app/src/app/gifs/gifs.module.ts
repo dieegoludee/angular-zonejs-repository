c261fe7df1be8ba73b745d3ca9d187ebf729073f
Diego Ludeña <diegolude.martin@gmail.com>
2024-04-23 13:16:08 +0200

    LazyLoader Image Component + Animaciones CSS

--- projects/03-gifs-app/src/app/gifs/gifs.module.ts
+++ projects/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,7 @@ import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
     CardListComponent,
     GifsCardComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
