33822c0c878aef76f6a58d20f1d61d778d1dad9a
Diego Ludeña <diegolude.martin@gmail.com>
2024-05-23 18:48:26 +0200

    Argumentos por URL - Página del país + switchMap - Refactorización

--- projects/04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ projects/04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) =>
+          this.countriesService.searchCountryByAlphaCode(id)
+        )
+      )
+      .subscribe((country) => {
+        if (!country) {
+          return this.router.navigateByUrl('');
+        }
+        console.log('TENEMOS UN PAÍS', country);
+        return;
+      });
+  }
 }
