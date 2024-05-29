5a15c11e86d533dae9e03a7f8dd46f0ffc8161ab
Diego Ludeña <diegolude.martin@gmail.com>
2024-05-29 18:17:45 +0200

    Implementación de Debounce de rxjs para peticiones cuando el usuario deja de escribir

--- projects/05-countryApp-Opt/src/app/shared/components/search-box/search-box.component.ts
+++ projects/05-countryApp-Opt/src/app/shared/components/search-box/search-box.component.ts
@@ -1,18 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+  private debouncer: Subject<string> = new Subject<string>();
+
   @Input()
   public placeholder: string = '';
 
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => this.onDebounce.emit(value));
+  }
+
   searchByCapital(value: string): void {
     if (value.length === 0) return;
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string): void {
+    this.debouncer.next(searchTerm);
   }
 }
