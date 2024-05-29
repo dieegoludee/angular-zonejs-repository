2510cbfdcbe6a0b2568bda9d9571250f9a169bcc
Diego Ludeña <diegolude.martin@gmail.com>
2024-05-29 19:14:59 +0200

    Buscar por región - Optimización

--- projects/05-countryApp-Opt/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ projects/05-countryApp-Opt/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,12 +11,24 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string): void {
-    this.countriesService.searchRegion(term).subscribe((countries) => {
+  searchByRegion(region: Region): void {
+    this.isLoading = true;
+    this.selectedRegion = region;
+    this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
