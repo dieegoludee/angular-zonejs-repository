cb668dbb50b404104b919b5a49fe0f749030dcac
Diego Ludeña <diegolude.martin@gmail.com>
2024-05-31 17:09:24 +0200

    Implementando PrimeNG - Menú superior

---
+++ projects/06-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  exports: [MenuModule, ButtonModule],
+})
+export class PrimeNgModule {}
