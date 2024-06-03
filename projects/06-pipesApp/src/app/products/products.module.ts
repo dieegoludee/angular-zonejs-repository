e0eb66c7584ca07aa817f0a3ed6a723f147837b0
Diego Ludeña <diegolude.martin@gmail.com>
2024-06-03 18:30:13 +0200

    UpperCase, LowerCase y TitleCase Pipes

--- projects/06-pipesApp/src/app/products/products.module.ts
+++ projects/06-pipesApp/src/app/products/products.module.ts
@@ -5,9 +5,14 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
-  declarations: [BasicsPageComponent, NumbersPageComponent, UncommonPageComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  declarations: [
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPageComponent,
+  ],
+  imports: [CommonModule, ProductsRoutingModule, PrimeNgModule],
 })
 export class ProductsModule {}
