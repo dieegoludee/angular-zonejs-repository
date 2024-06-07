4444104092b22006c858b6fa459d217d5a7d93bc
Diego Ludeña <diegolude.martin@gmail.com>
2024-06-07 17:52:15 +0200

    Implementando i18nSelect Pipe

--- projects/06-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ projects/06-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Diego';
+  public gender: 'male' | 'female' = 'male';
 
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 }
