64b26a4376320c4b66efd16d0c49ad11e59bb6d6
Diego Ludeña <diegolude.martin@gmail.com>
2024-06-18 20:00:04 +0200

    HeroesService - Traer información de los héroes del json-server

--- projects/07-heroesApp/src/app/app.module.ts
+++ projects/07-heroesApp/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
